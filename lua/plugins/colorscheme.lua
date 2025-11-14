6165ff63cd4dd94ef5db492d8655fab4f03e4d6e
SohamPrabhu <prabhusoham151@gmail.com>
2025-11-13 23:34:20 -0500

    Changed the scheme to default cause sudu is complaingin and you know how he gets

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- Install the Nord colorscheme
+
+  -- Override LazyVim’s default colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "default",
+    },
+  },
+}
