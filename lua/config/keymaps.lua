a8cba7f44fd86efe9882834d55566d9d45ed9b66
SohamPrabhu <prabhusoham151@gmail.com>
2025-12-05 15:19:29 -0500

    fixed bug in the lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
 
-vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
-vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete without yanking" })
-vim.keymap.set({ "n", "v" }, "c", '"_c', { desc = "Change without yanking" })
+vim.keymap.set({ "n", "v" }, "d", '"_d')
+vim.keymap.set({ "n", "v" }, "x", '"_x')
+vim.keymap.set({ "n", "v" }, "c", '"_c')
