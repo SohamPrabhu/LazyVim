083eb145bbc1402507db31680e2d5dae34504c41
SohamPrabhu <prabhusoham151@gmail.com>
2026-05-08 00:22:09 -0400

    All changes

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -6,7 +6,20 @@ return{
     config = function()
       require("telescope").setup({})
     end
-  }
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({})
+    end,
+
+  },
 
 
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,8 @@ return{
     "nvim-treesitter/nvim-treesitter",
     build = "TSUpdate",
     config = function()
-      require("nvim-treesitter.config").setup({
-        ensured_installed = {"lua","python","javascript","java","typescript","html","c","css","cpp","markdown","dockerfile","json"},
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {"lua","python","javascript","java","typescript","html","c","css","cpp","markdown","dockerfile","json"},
         auto_install = true,
         highlight ={enable = true,},
         indent={enable = true,},
